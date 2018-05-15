1f0c58499f89a0ad601adc18f10e98f9bb2b64cb
xuejian.xu <jearbilove@yeah.net>
2018-05-15 10:16:26 +0800

    update

--- app/src/store/models/AppModel.ts
+++ app/src/store/models/AppModel.ts
@@ -1,21 +1,21 @@
-import {observable, action, IObservableArray} from 'mobx'
+import { observable, action, IObservableArray } from 'mobx'
 import ApiService from '@src/utils/ApiService'
 
 // types
-import {IFile} from '@src/types/IFile'
-import {IResponse} from '@src/types/IResponse'
+import { IFile } from '@src/types/IFile'
+import { IResponse } from '@src/types/IResponse'
 
 class AppModel {
     @observable current: IFile | null
     @observable files: IObservableArray<IFile>
 
-    constructor(){
+    constructor() {
         this.files = observable([])
         this.current = null
     }
 
     @action
-    async loadFiles(){
+    async loadFiles() {
         const res = await ApiService.get<IResponse<Array<IFile>>>('//localhost:4000/api/file/all')
 
         this.files.replace(res.data.d)
