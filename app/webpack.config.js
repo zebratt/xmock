345ae7624e22ce4f0d4913154e83b072929dbda5
xuejian.xu <jearbilove@yeah.net>
2018-05-31 17:28:59 +0800

    update config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,15 +1,11 @@
 'use strict'
 
-/* eslint no-console: "off" */
 const webpackConfigs = require('./config')
 const defaultConfig = 'dev'
 
 module.exports = configName => {
-    // If there was no configuration give, assume default
     const requestedConfig = configName || defaultConfig
 
-    // Return a new instance of the webpack config
-    // or the default one if it cannot be found.
     let LoadedConfig = defaultConfig
 
     if (webpackConfigs[requestedConfig] !== undefined) {
@@ -25,7 +21,6 @@ module.exports = configName => {
 
     const loadedInstance = new LoadedConfig()
 
-    // Set the global environment
     process.env.NODE_ENV = loadedInstance.env
 
     return loadedInstance.config
